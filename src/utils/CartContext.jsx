cdc4840d241252932bf015ccc4e475da6a65c8de
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-06 17:23:56 +0530

    initial commit

--- src/utils/CartContext.jsx
+++ src/utils/CartContext.jsx
@@ -8,19 +8,35 @@ export const CartProvider = ({ children }) => {
           const exist = bookcart.find((item) => item.book.id === book.id);
           exist
                ? setBookCart((prev) =>
-                      prev.map((item) =>
-                           item.book.id === book.id
-                                ? { ...item, book, quantity: item.quantity + 1 }
-                                : item
-                      )
-                 )
+                    prev.map((item) =>
+                         item.book.id === book.id
+                              ? { ...item, book, quantity: item.quantity + 1 }
+                              : item
+                    )
+               )
                : setBookCart((prev) => [...prev, { book, quantity: 1 }]);
      };
      const removeBook = (id) => {
           setBookCart((prev) => prev.filter((item) => item.book.id !== id));
      };
-     const increaseQuantity = (id) => {};
-     const decreaseQuantity = (id) => {};
+     const increaseQuantity = (id) => {
+          setBookCart((prev) =>
+               prev.map((item) =>
+                    item.book.id === id
+                         ? { ...item, quantity: item.quantity + 1 }
+                         : item
+               )
+          );
+     };
+     const decreaseQuantity = (id) => {
+          setBookCart((prev) =>
+               prev.map((item) =>
+                    item.book.id === id
+                         ? { ...item, quantity: item.quantity - 1 }
+                         : item
+               ).filter((item)=>item.quantity>0)
+          );
+     };
      return (
           <CartContext.Provider
                value={{
