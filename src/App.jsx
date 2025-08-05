9c0efee0887e6ca3682c8c77caee385d590ff8ae
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-08-05 22:58:26 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Header from './components/Header'
-import BookList from './components/BookList'
-import Cart from './components/Cart'
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <BookList/>
-      <Cart/>
-    </div>
+    <div>App</div>
   )
 }
 
