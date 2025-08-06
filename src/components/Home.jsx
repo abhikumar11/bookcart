51cdea1ec069c2ac3e1d8cf4d00198b66877d055
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-08-07 00:11:23 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import BookCard from './BookCard'
+import { bookStore } from '../utils/BookContext'
 
 const Home = () => {
+  const {loading,book}=useContext(bookStore);
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home Page</h1>
+      {loading?"Loading":<BookCard book={book}/>}
+    </div>
   )
 }
+
 
 export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import UserProvider from './utils/UserContext.jsx'
+import BookProvider from './utils/BookContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <UserProvider>
+      <BookProvider>
     <App/>
+    </BookProvider>
     </UserProvider>
     </BrowserRouter>
   </StrictMode>,
